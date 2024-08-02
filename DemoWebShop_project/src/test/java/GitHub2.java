535e1f157e0424874054cfcddfb1830c9a7e7bce
User <User@DESKTOP-RG04IRQ>
2024-08-02 15:04:06 +0530

    Added Github2 ForkRepo

--- DemoWebShop_project/src/test/java/GitHub2.java
+++ DemoWebShop_project/src/test/java/GitHub2.java
@@ -4,7 +4,7 @@ public class GitHub2 {
 	public static void main(String[] args)
 	
 	{
-		System.out.println("Added Github2 class to git	");
+		System.out.println("Added Github2 class to git Forkrepository1	");
 	}
 
 }
